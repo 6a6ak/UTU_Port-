c88a00182399a16e2f68483ec70eb6ee43644956
6a6ak <babak.ahari@gmail.com>
2023-12-13 23:33:07 +0100

    opened port view

--- port+.sh
+++ port+.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
-# Prompt the user to enter the port number
-read -p "Enter the port number you want to open: " port
+# Function to open a port
+open_port() {
+    sudo iptables -A INPUT -p tcp --dport $1 -j ACCEPT
+    echo "Port $1 has been opened."
+}
 
-# Open the specified port using iptables
-sudo iptables -A INPUT -p tcp --dport $port -j ACCEPT
+# Function to close a port
+close_port() {
+    sudo iptables -D INPUT -p tcp --dport $1 -j ACCEPT
+    echo "Port $1 has been closed."
+}
+
+# Function to list open ports
+list_ports() {
+    echo "List of open ports:"
+    sudo netstat -tuln
+}
+
+# Main script starts here
+echo "Do you want to open, close, or list open ports?"
+select opt in "Open" "Close" "List" "Exit"; do
+    case $opt in
+        Open )
+            read -p "Enter the port number to open: " port
+            open_port $port
+            break
+            ;;
+        Close )
+            read -p "Enter the port number to close: " port
+            close_port $port
+            break
+            ;;
+        List )
+            list_ports
+            break
+            ;;
+        Exit )
+            break
+            ;;
+        * ) echo "Invalid option $REPLY";;
+    esac
+done
 
 # Save the iptables rules (optional, uncomment if needed)
 # sudo iptables-save
-
-echo "Port $port has been opened."
